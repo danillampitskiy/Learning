ef6b8c64bc10e8253d7ff6fafa3f8043253a9823
danillampitskiy <lampitsky.danil1@gmail.com>
2023-09-19 11:00:18 +0200

    add 'Data structures homework' solution

---
+++ Homeworks/Hw1/Part1/hw1.py
@@ -0,0 +1,32 @@
+"""
+Given an array of size n, find the most common and the least common elements.
+The most common element is the element that appears more than n // 2 times.
+The least common element is the element that appears fewer than other.
+
+You may assume that the array is non-empty and the most common element
+always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3, 2
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2, 1
+
+"""
+from typing import List, Tuple
+
+
+def major_and_minor_elem(inp: List) -> Tuple[int, int]:
+    counts = {}  # Dictionary to store element counts
+
+    for elem in inp:
+        counts[elem] = counts.get(elem, 0) + 1
+
+    most_common = max(counts, key=counts.get)
+    least_common = min(counts, key=counts.get)
+
+    return most_common, least_common
